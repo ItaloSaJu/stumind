cd2ad20526aa380bafc32e93ec25e47c3352d2d7
Italo Salas <italosalas63@gmail.com>
2024-10-17 00:14:51 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'stumind',
+  webDir: 'www'
+};
+
+export default config;
